44bd6e9c5d6075fef9dd51cff96f0ec1c90b94a2
Dion Lucca <120736459+odjion@users.noreply.github.com>
2026-09-04 23:52:15 -0300

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+
+int idade = 0;
+float peso = 0.0;
+
+ printf("Digite a idade:");
+ scanf("%d", &idade);
+ 
+ 
+ printf("Digite o peso:");
+ scanf("%f", &peso);
+ 
+ 
+ printf("Idade atual: %d\n", idade);
+ printf("pes: %.2f\n", peso);
+ 
+    return 0;
+}
